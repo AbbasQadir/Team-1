926b94053cb087228b5655f868f6651d11dac6ad
joecollins783 <230184158@aston.ac.uk>
2024-11-29 13:42:59 +0000

    Added product page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/productList', function () {
+    return view('products');
+});
+
+Route::get('/product', function () {
+    return view('specificProduct');
+});
