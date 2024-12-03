a89a0de14e6947c490cb4c57500cc825f4c59ec8
M-Asad-K <230126310@aston.ac.uk>
2024-12-03 15:42:14 +0000

    Add files via upload

--- remove_from_basket.php
+++ remove_from_basket.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['basket_id'])) {
     $stmt = $db->prepare("DELETE FROM asad_basket WHERE basket_id = :basket_id AND user_id = :user_id");
     $stmt->execute([
         ':basket_id' => $basketId,
-        ':user_id' => $_SESSION['uid'] // Updated session variable
+        ':user_id' => $_SESSION['uid'] 
     ]);
 
     header("Location: Basket.php");
